3cd68f1629079e54bee320e25416a00c5d7b3f6d
dustin <dselman@gmail.com>
2015-04-23 16:20:53 -0700

    added oscdump exec

---
+++ entoptic-sim/CMakeFiles/OscDump.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OscDump.dir/OscDump.cpp.o"
+  "OscDump.pdb"
+  "OscDump"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/OscDump.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
