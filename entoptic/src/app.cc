4c6cd09d9398c2ae23e7045199aed7afb3045486
Bryan Culbertson <bryan.culbertson@gmail.com>
2015-05-03 18:01:29 -0700

    open cv diff masking

--- entoptic/src/app.cc
+++ entoptic/src/app.cc
@@ -1,119 +1,84 @@
 #include <stdio.h>
+#include <cv.h>
+#include <highgui.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string>
-#include <opencv2/opencv.hpp>
-#include <sstream>
+#include "opencv2/video/tracking.hpp"
+#include <math.h>
+#include <time.h>
 
-#include "osc/OscOutboundPacketStream.h"
-#include "ip/UdpSocket.h"
+float MHI_DURATION = 0.05;
+int DEFAULT_THRESHOLD = 32;
+float MAX_TIME_DELTA = 12500.0;
+float MIN_TIME_DELTA = 5;
 
-#define OUTPUT_BUFFER_SIZE 1024 * 4
+int main (int argc, char** argv) {
+    cv::namedWindow("motion", CV_WINDOW_AUTOSIZE);
 
-#if CV_MAJOR_VERSION < 3
-  #define CAP_PROP_FRAME_WIDTH CV_CAP_PROP_FRAME_WIDTH
-  #define CAP_PROP_FRAME_HEIGHT CV_CAP_PROP_FRAME_HEIGHT
-#endif
-
-std::string IntToString (int a)
-{
-    std::ostringstream temp;
-    temp<<a;
-    return temp.str();
-}
-
-std::string FloatToString (float a)
-{
-    std::ostringstream temp;
-    temp<<a;
-    return temp.str();
-}
-
-int main(int argc, char* argv[]) {
-
-    if (argc >= 2 && std::strcmp(argv[1], "-h") == 0) {
-        std::cout << "usage: entoptic [hostname [port]]\n";
-        return 0;
-    }
-
-    const char* address = "127.0.0.1";
-    if (argc >= 2)
-        address = argv[1];
-
-    int port = 7000;
-    if (argc >= 3)
-        port = std::atoi(argv[2]);
-
-    // OSC setup
-    UdpTransmitSocket transmitSocket(IpEndpointName(address, port));
-    char buffer[OUTPUT_BUFFER_SIZE];
-    osc::OutboundPacketStream p(buffer, OUTPUT_BUFFER_SIZE);
-
-    // open the video camera no. 0
     cv::VideoCapture cap(0);
-
-    // if not success, exit program
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     if (!cap.isOpened()) {
-        std::cout << "Cannot open the video cam" << std::endl;
+        std::cerr << "Cannot open camera" << std::endl;
         return -1;
     }
 
-    // get the width of frames of the video
-    double dWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-    // get the height of frames of the video
-    double dHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    cv::Mat frame, frame_diff, gray_diff, motion_mask;
+    cap.read(frame);
+    cv::Size frame_size = frame.size();
+    int h = frame_size.height;
+    int w = frame_size.width;
+    double timestamp = 1000.0 * clock() / CLOCKS_PER_SEC;
+    cv::Mat prev_frame = frame.clone();
+    cv::Mat motion_history(h, w, CV_32FC1, cv::Scalar(0,0,0));
+    cv::Mat mg_mask(h, w, CV_8UC1, cv::Scalar(0,0,0));
+    cv::Mat mg_orient(h, w, CV_32FC1, cv::Scalar(0,0,0));
+    cv::Mat seg_mask(h, w, CV_32FC1, cv::Scalar(0,0,0));
+    std::vector<cv::Rect> seg_bounds;
+    cv::Mat vis(h, w, CV_32FC3);
+    cv::Mat silh_roi, orient_roi, mask_roi, mhi_roi;
 
-    std::cout << "Frame size : " << dWidth << " x " << dHeight << std::endl;
-
-    // create a window called "MyVideo"
-    cv::namedWindow("MyVideo", cv::WINDOW_AUTOSIZE); 
-
-    std::string osc_address_str;
-    std::string gridData;
-    float activation;
-    while (true) {
-        cv::Mat frame;
-        // read a new frame from video
-        bool bSuccess = cap.read(frame);
-        // if not success, break loop
-        if (!bSuccess) { 
-            std::cout << "Cannot read a frame from video stream" << std::endl;
+    while (1) {
+        cap.read(frame);
+        if(!frame.data) {
+            std::cout << "camera stopped" << std::endl;
             break;
         }
 
-        // show the frame in "MyVideo" window
-        imshow("MyVideo", frame); 
+        cv::absdiff(frame, prev_frame, frame_diff);
+        cv::cvtColor(frame_diff, gray_diff, CV_BGR2GRAY);
+        cv::threshold(gray_diff, motion_mask, DEFAULT_THRESHOLD, 255, 0);
+        timestamp = 1000.0 * clock() / CLOCKS_PER_SEC;
+        cv::updateMotionHistory(motion_mask, motion_history, timestamp,
+                                MHI_DURATION);
+        cv::calcMotionGradient(motion_history, mg_mask, mg_orient,
+                               MIN_TIME_DELTA, MAX_TIME_DELTA, 3);
+        cv::segmentMotion(motion_history, seg_mask, seg_bounds, timestamp, 32);
 
-        // OSC stuff
-        
-        gridData = "";
-        for (int i = 1; i < 21; ++i)
-        {
-            for (int j = 1; j < 11; ++j)
-            {
-                activation = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-                if (activation < .7) {
-                    gridData += "0,";
-                } 
-                else {
-                    gridData += FloatToString(activation) + ",";
+        for (int i = 0; i < seg_bounds.size(); i++) {
+            const cv::Rect& rec = seg_bounds[i];
+            if (rec.area() > 5000 && rec.area() < 70000) {
+                silh_roi = motion_mask(rec);
+                orient_roi = mg_orient(rec);
+                mask_roi = mg_mask(rec);
+                mhi_roi = motion_history(rec);
+                if (norm(silh_roi, cv::NORM_L2, cv::noArray()) > rec.area()*0.5) {
+                    double angle = cv::calcGlobalOrientation(
+                        orient_roi, mask_roi, mhi_roi,timestamp, MHI_DURATION);
                 }
-                
             }
         }
 
-        p.Clear();
-        p << osc::BeginMessage("/A/C1") 
-            << gridData.substr(0, gridData.size()-1).c_str()
-            << osc::EndMessage;
-    
-        transmitSocket.Send(p.Data(), p.Size());
-
-        // wait for 'esc' key press for 30ms.
-        // If 'esc' key is pressed, break loop
-        if (cv::waitKey(30) == 27) { 
+        cv::imshow("motion", vis);
+        prev_frame = frame.clone();
+        if (cv::waitKey(30) >= 0) {
             std::cout << "esc key is pressed by user" << std::endl;
             break; 
         }
     }
+
     return 0;
 }
